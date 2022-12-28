2b394970eba47cfd1030242f500dd39364355bf2
Ahmed Onour <ahmedonour.49@gmail.com>
2022-12-28 21:04:01 +0200

    test

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,4 @@
 import { expect, test } from '@playwright/test';
+import HeroButton from '../src/routes/project'
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('p')).toBe('Ahmed Suliman');
-});
+test()
