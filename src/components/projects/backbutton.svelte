dcb90dda159c748008af42ebe969a6708270249b
Ahmed Onour <ahmedonour.49@gmail.com>
2023-01-22 15:17:02 +0200

    UPDATE !: Image, fixing some bugs

--- src/components/projects/backbutton.svelte
+++ src/components/projects/backbutton.svelte
@@ -1,18 +1,22 @@
-<a href="/project" class="backButton">Back</a>
+<a href="/project" class="backButton"><i class="fas fa-arrow-circle-left"></i></a>
 
 <style>
 	.backButton {
 		color: white;
-		font-size: 3rem;
+		font-size: 4rem;
 		cursor: pointer;
 		position: sticky;
 		left: 145px;
 		top: 0px;
+		transition: all 500ms ease-in-out;
+	}
+	.backButton:hover{
+		color: black;
 	}
 	@media screen and (max-width: 768px) {
 		.backButton {
 			left: 20px;
-			font-size: 2rem;
+			font-size: 3rem;
 			font-weight: 700;
 			margin-bottom: 3rem;
 		}
